9083be878837615d5f161a6ee2a8e0462e321506
Coolbuyq <aniket.nagpure9970@gmail.com>
2022-02-12 22:38:46 +0530

    changereee

---
+++ MiniProject_C/3_Implementation/inc/advcalculator.h
@@ -0,0 +1,31 @@
+/**
+ * @file advcalculator.h
+ * @author your name (you@domain.com)
+ * @brief 
+ * @version 0.1
+ * @date 2022-02-12
+ * 
+ * @copyright Copyright (c) 2022
+ * 
+ */
+
+#ifndef ADVCALCULATOR_H_
+#define ADVCALCULATOR_H_
+
+#include <stdio.h>
+#include<stdlib.h>
+#include<math.h>
+int addition(int a,int b);
+int subtraction(int a,int b);
+float multiplication(int a,int b);
+float division(int a,int b);
+int modulus(int a,int b);
+float logarithmic(float a);
+int p(int x, int n);
+int p1(int x, int n);
+int p2(int x, int n);
+float trignometric(int op,float val);
+float exponential(float a,float b);
+float squareroot(float b);
+
+#endif
