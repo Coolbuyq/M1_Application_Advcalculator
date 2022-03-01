d9aea01bc19eb3c4d702c090a614dd55fbe73936
Coolbuyq <aniket.nagpure9970@gmail.com>
2022-03-01 21:09:43 +0530

    make

--- 3_Implementation/include/advcalculator.h
+++ 3_Implementation/include/advcalculator.h
@@ -1,7 +1,7 @@
 /**
  * @file advcalculator.h
- * @author your name (you@domain.com)
- * @brief 
+ * @author Aniket Nagpure
+ * @brief Our project is smart calculator that performs arithmetic operations on numbers. The simplest calculators can do only addition, subtraction, multiplication, and division. More sophisticated calculators can handle exponent like exponential operations, logarithm s, trigonometric functions, and modulus.
  * @version 0.1
  * @date 2022-02-12
  * 
@@ -15,17 +15,99 @@
 #include <stdio.h>
 #include<stdlib.h>
 #include<math.h>
+/**
+ * @brief Here this function is going to add twonumbers
+ * 
+ * @param a 
+ * @param b 
+ * @return int 
+ */
 int addition(int a,int b);
+/**
+ * @brief Here this function is going to substrct two numbers
+ * 
+ * @param a 
+ * @param b 
+ * @return int 
+ */
 int subtraction(int a,int b);
+/**
+ * @brief Here this function is going to multiply two numbers
+ * 
+ * @param a 
+ * @param b 
+ * @return float 
+ */
 float multiplication(int a,int b);
+/**
+ * @brief Here this function is going to divide two numbers
+ * 
+ * @param a 
+ * @param b 
+ * @return float 
+ */
 float division(int a,int b);
+/**
+ * @brief Here this function is going to give modules of  two numbers
+ * 
+ * @param a 
+ * @param b 
+ * @return int 
+ */
 int modulus(int a,int b);
+/**
+ * @brief Here this function is going to give log of number
+ * 
+ * @param a 
+ * @return float 
+ */
 float logarithmic(float a);
+/**
+ * @brief Here this function is going to give power to numbers
+ * 
+ * @param x 
+ * @param n 
+ * @return int 
+ */
 int p(int x, int n);
+/**
+ * @brief Here this function is going to give square of  two numbers
+ * 
+ * @param x 
+ * @param n 
+ * @return int 
+ */
 int p1(int x, int n);
+/**
+ * @brief Here this function is going to givw cube of number
+ * 
+ * @param x 
+ * @param n 
+ * @return int 
+ */
 int p2(int x, int n);
+/**
+ * @brief Here this function is going to give trignometric of numbers
+ * 
+ * @param op 
+ * @param val 
+ * @return float 
+ */
 float trignometric(int op,float val);
+/**
+ * @brief Here this function is going to give exponetial
+ * 
+ * @param a 
+ * @param b 
+ * @return float 
+ */
 float exponential(float a,float b);
+/**
+ * @brief Here this function is going to give squareroot of number
+ * 
+ * @param b 
+ * @return float 
+ */
 float squareroot(float b);
 
 #endif
